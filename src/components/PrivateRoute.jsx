181bd2f3e0366833b586ec2fe57f510f5ac29064
unknown <freelancer.bstha@gmail.com>
2025-01-17 15:01:17 +0545

    Final Code for PH Production

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+
+const PrivateRoute = () => {
+    const { verifyToken } = useContext(UserContext);
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            const isValid = await verifyToken(); 
+            setIsAuthenticated(isValid); 
+        };
+
+        checkAuthentication();
+    }, [verifyToken]); 
+
+    if (isAuthenticated === null) {
+        return <div>Loading...</div>;
+    }
+
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
